b40a5b222a132a5f5c6959b1a86f02d6e73410f8
RWTH-MK <marcel.kaiser1@live.com>
2024-05-18 13:46:36 +0200

    Voltcraft 220 Messbereiche vervollständigt

--- multimeter.py
+++ multimeter.py
@@ -35,7 +35,9 @@ def _multimeter_fehler(daten, messbereich, einheitenpotenz, grundgenauigkeit, dg
     return unp.uarray(daten_konvert, fehler_liste)
 
 
-def voltcraft220_2Vgleich(daten):
+def _voltcraft220(daten, messbereich, einheitenpotenz, grundgenauigkeit, dgts):
+    return _multimeter_fehler(daten, messbereich, einheitenpotenz, grundgenauigkeit, dgts, 2000)
+def voltcraft220_200mVgleich(daten):
     """Gibt einen uarray zurück mit den Messwerten und den Fehlern des Voltcraft 220 für den Messbereich 2V Gleichstrom.
     Herstellerangaben entnommen aus (06.05.24):
     https://www.manualslib.de/manual/349243/Voltcraft-Vc-220.html?page=14#manual
@@ -43,14 +45,37 @@ def voltcraft220_2Vgleich(daten):
     Parameters
     ----------
     daten : array_like
-        Liste oder Array der Messwerte
+        Liste oder Array der Messwerte in der abgelesenen Einheit
 
     Returns
     -------
     uarray
+        In der Basiseinheit
     """
-    return _multimeter_fehler(daten, 2, 1, 0.6, 5, 2000)
+    return _voltcraft220(daten, 200, 1e-3, 0.6, 5)
+def voltcraft220_2Vgleich(daten):
+    return _voltcraft220(daten, 2, 1, 0.6, 5)
 def voltcraft220_20Vgleich(daten):
-    return _multimeter_fehler(daten, 20, 1, 0.6, 5, 2000)
+    return _voltcraft220(daten, 20, 1, 0.6, 5)
+def voltcraft220_200Vgleich(daten):
+    return _voltcraft220(daten, 200, 1, 0.6, 5)
+def voltcraft220_1000Vgleich(daten):
+    return _voltcraft220(daten, 1000, 1, 1, 5)
+def voltcraft220_2Vwechsel(daten):
+    return _voltcraft220(daten, 2, 1, 1, 5)
+def voltcraft220_20Vwechsel(daten):
+    return _voltcraft220(daten, 20, 1, 1, 5)
+def voltcraft220_200Vwechsel(daten):
+    return _voltcraft220(daten, 200, 1, 1, 5)
+def voltcraft220_750Vwechsel(daten):
+    return _voltcraft220(daten, 750, 1, 1.2, 5)
+def voltcraft220_200muA(daten):
+    return _voltcraft220(daten, 200, 1e-6, 1, 2)
+def voltcraft220_2mA(daten):
+    return _voltcraft220(daten, 2, 1e-3, 1, 2)
 def voltcraft220_20mA(daten):
-    return _multimeter_fehler(daten, 20, 1e-3, 1, 2, 2000)
+    return _voltcraft220(daten, 20, 1e-3, 1, 2)
+def voltcraft220_200mA(daten):
+    return _voltcraft220(daten, 200, 1e-3, 1, 2)
+def voltcraft220_20A(daten):
+    return _voltcraft220(daten, 0, 1, 1.2, 8)
